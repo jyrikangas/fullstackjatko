d50cd1c16651a84d04c63e8a61e08251f0b5d1a1
Jyri Kangas <jyri_kangas@outlook.com>
2026-05-08 15:28:59 +0300

    Auto-save: 2026-05-08T12:28:59.931805Z

--- 6/anecdotes/src/components/AnecdoteForm.jsx
+++ 6/anecdotes/src/components/AnecdoteForm.jsx
@@ -1,10 +1,10 @@
 import { useAnecdoteActions } from "../store"
 
 const AnecdoteForm = () => {
-const { add } = useAnecdoteActions()
+    const { add } = useAnecdoteActions()
 
-  const generateId = () => Number((Math.random() * 1000000).toFixed(0))
-  const addAnecdote = (e) => {
+    const generateId = () => Number((Math.random() * 1000000).toFixed(0))
+    const addAnecdote = (e) => {
     e.preventDefault()
     const content = e.target.anecdote.value
     add({ id: generateId(), content, votes: 0})
