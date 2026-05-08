fb2c049085c0107fa85a3d62fc167df61a9ab81a
Jyri Kangas <jyri_kangas@outlook.com>
2026-05-08 15:40:29 +0300

    autocom 20260508-1240 App.jsx

--- 6/anecdotes/src/App.jsx
+++ 6/anecdotes/src/App.jsx
@@ -2,29 +2,12 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 const App = () => {
-  const anecdotes = useAnecdotes()
-  const { vote, add } = useAnecdoteActions()
 
-  const generateId = () => Number((Math.random() * 1000000).toFixed(0))
-  const addAnecdote = (e) => {
-    e.preventDefault()
-    const content = e.target.anecdote.value
-    add({ id: generateId(), content, votes: 0})
-    e.target.reset()
-  }
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes} votes 
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      ))}
-      
+      <AnecdoteList/>
+      <AnecdoteForm/>
     </div>
   )
 }
