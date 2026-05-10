a11efe4113061314d4bc7ddca02bf73029c5822d
Jyri Kangas <jyri_kangas@outlook.com>
2026-05-10 14:06:07 +0300

    autocom 20260510-1106 App.jsx

--- 6/anecdotes/src/App.jsx
+++ 6/anecdotes/src/App.jsx
@@ -1,9 +1,14 @@
 import { useEffect } from 'react'
+
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
+import Notification from './components/Notification'
+
 import { useAnecdoteActions } from './store'
 import anecdoteService from './services/anecdotes'
+
+
 const App = () => {
 
   const { initialize } = useAnecdoteActions()
