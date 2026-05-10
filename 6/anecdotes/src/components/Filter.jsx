c6c9d359441bd1ada9ae0868408992023762a932
Jyri Kangas <jyri_kangas@outlook.com>
2026-05-10 10:05:13 +0300

    autocom 20260510-0705 Filter.jsx

--- 6/anecdotes/src/components/Filter.jsx
+++ 6/anecdotes/src/components/Filter.jsx
@@ -1,6 +1,9 @@
+import { useAnecdoteActions } from "../store"
 const Filter = () => {
+  const { setFilter } = useAnecdoteActions()
   const handleChange = (event) => {
-    // input-kentän arvo muuttujassa event.target.value
+    setFilter(event.target.value)
+    
   }
   const style = {
     marginBottom: 10
