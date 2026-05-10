ab63630f5b800d030fc28e9634b81153f8a0c9c8
Jyri Kangas <jyri_kangas@outlook.com>
2026-05-10 14:01:28 +0300

    autocom 20260510-1101 notificationStore.js

--- 6/anecdotes/src/notificationStore.js
+++ 6/anecdotes/src/notificationStore.js
@@ -1,33 +1,17 @@
 
 import { create } from 'zustand'
 
-let hideTimeoutId
-
-const AUTO_HIDE_MS = 5000
-
 const useNotificationStore = create((set) => ({
-    message: '',
-    visible: false,
+    message: "Notification message",
+    duration: 5,
+    visible: true,
     actions: {
-        showNotification: (message, duration = AUTO_HIDE_MS) => {
-            if (hideTimeoutId) {
-                clearTimeout(hideTimeoutId)
-            }
+        setMessage: message => set(
+            state 0> ({message: message })
+        ),
+        makeVisible: => set(
+            state => ({ visible: true})
+        )
 
-            set({ message, visible: true })
-
-            hideTimeoutId = setTimeout(() => {
-                set({ visible: false })
-                hideTimeoutId = undefined
-            }, duration)
-        },
-        hideNotification: () => {
-            if (hideTimeoutId) {
-                clearTimeout(hideTimeoutId)
-                hideTimeoutId = undefined
-            }
-
-            set({ visible: false })
-        }
     }
 }))
