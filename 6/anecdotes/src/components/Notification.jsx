e8da3314ed5c09a59c4debbbc59f154718a6a960
Jyri Kangas <jyri_kangas@outlook.com>
2026-05-10 14:11:57 +0300

    autocom 20260510-1111 Notification.jsx

--- 6/anecdotes/src/components/Notification.jsx
+++ 6/anecdotes/src/components/Notification.jsx
@@ -2,12 +2,13 @@
 import { useNotifications } from "../notificationStore"
 
 const Notification = () => {
-  const notification = useNotifications()
+  const { notification, duration } = useNotifications()
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1,
-    marginBottom: 10
+    marginBottom: 10,
+    transition: 'all 500ms linear 5s'
   }
 
   return <div style={style}> { notification }</div>
