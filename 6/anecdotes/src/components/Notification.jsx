c3fdcd1afe36a340b9e7b4ecbbc2fe3fca6e87de
Jyri Kangas <jyri_kangas@outlook.com>
2026-05-10 14:16:57 +0300

    autocom 20260510-1116 Notification.jsx

--- 6/anecdotes/src/components/Notification.jsx
+++ 6/anecdotes/src/components/Notification.jsx
@@ -11,7 +11,7 @@ const Notification = () => {
     
   }
 
-  return <div style={style}> text { message.message } </div>
+  return <div style={style}> text { message } </div>
 }
 
 export default Notification
