4774bfff4904c543f3434325512e2e6d1d1f4b97
Jyri Kangas <jyri_kangas@outlook.com>
2026-05-12 14:36:56 +0300

    Tuesday 14:36 store.js

--- 6/anecdotes/src/store.js
+++ 6/anecdotes/src/store.js
@@ -44,6 +44,7 @@ const useAnecdoteStore = create((set) => ({
 
 }))
 
+export default useAnecdoteStore 
 export const useAnecdotes = () => {
   const anecdotes = useAnecdoteStore((state) => state.anecdotes)
   const filter = useAnecdoteStore((state) => state.filter)
