72f1cefd3cee014953f9eb75621149336fbdf4ef
Jyri Kangas <jyri_kangas@outlook.com>
2026-05-13 17:28:29 +0300

    Wednesday 17:28 useAnecdotes.js

--- 6/query-anecdotes/src/useAnecdotes.js
+++ 6/query-anecdotes/src/useAnecdotes.js
@@ -3,17 +3,7 @@ import { useQuery, useQueryClient } from '@tanstack/react-query'
 export const useAnecdotes = () => {
     const queryClient = useQueryClient()
 
-    const result = useQuery({
-        queryKey:['anecdotes'],
-        queryFn: async () => {
-            const response = await fetch('http://localhost:3001/anecdotes')
-            return response.json()
-        }
-    })
-    console.log(JSON.parse(JSON.stringify(result)))
-    if (result.isPending) {
-        return []
-    };
+    
     
     return result.data
 }
