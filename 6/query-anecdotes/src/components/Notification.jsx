ba9bfacc2f037e0b02eb4b5f4fbf19b763e6bf6c
Jyri Kangas <jyri_kangas@outlook.com>
2026-05-14 20:38:27 +0300

    Thursday 20:38 Notification.jsx

--- 6/query-anecdotes/src/components/Notification.jsx
+++ 6/query-anecdotes/src/components/Notification.jsx
@@ -1,16 +1,20 @@
+import { useContext } from "react"
+import NotificationContext from "../NotificationContext"
+
 const Notification = () => {
+  const { notification, setNotification } = useContext(NotificationContext)
+
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1,
     marginBottom: 5
   }
-  
-  if (true) return null
+
 
   return (
     <div style={style}>
-      
+      {notification}
     </div>
   )
 }
