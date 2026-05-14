adf25ec2dbb0c48173fc169dc8ea14fcab38471d
Jyri Kangas <jyri_kangas@outlook.com>
2026-05-14 20:36:25 +0300

    Thursday 20:36 main.jsx

--- 6/query-anecdotes/src/main.jsx
+++ 6/query-anecdotes/src/main.jsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { NotificationContextProvider } from './NotificationContext'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
