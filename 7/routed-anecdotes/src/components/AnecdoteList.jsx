41707792d4c7a3887c34bbc18b41628c0e52eb3b
Jyri Kangas <jyri_kangas@outlook.com>
2026-07-05 21:18:47 +0300

    Sunday 21:18 AnecdoteList.jsx

--- 7/routed-anecdotes/src/components/AnecdoteList.jsx
+++ 7/routed-anecdotes/src/components/AnecdoteList.jsx
@@ -1,8 +1,12 @@
-const AnecdoteList = ({ anecdotes }) => (
+import { useAnecdotes } from "../hooks"
+const AnecdoteList = ({ anecdotes }) => {
+  const { deleteAnecdote } = useAnecdotes
+  return (
   <div>
     <h2>Anecdotes</h2>
     <ul>
       {anecdotes.map(anecdote => <li key={anecdote.id}>{anecdote.content}</li>)}
+      <button type='button' onClick={deleteAnecdote(anecdote)}>delete</button>
     </ul>
   </div>
 )
